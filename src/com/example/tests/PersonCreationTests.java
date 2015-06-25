74dbc62bff72b0ebbbdc442192da075923bf71b1
maskaradm <maskaradm@gmail.com>
2015-06-25 14:54:54 +0300

    Task 2 (with some random values)

--- src/com/example/tests/PersonCreationTests.java
+++ src/com/example/tests/PersonCreationTests.java
@@ -9,21 +9,28 @@ public class PersonCreationTests extends TestBase {
 		openMainPage();
 		gotoAddNewPage();
 		PersonData person = new PersonData();
-		person.firstname="Name";
-		person.lastname="Surname";
-		person.address="City and street";
-		person.home="569825";
-		person.mobile="29588991";
-		person.work="744115";
-		person.email="test@mail.com";
-		person.email2="test2@mail.com";
-		person.bday="28";
-		person.bmonth="February";
-		person.byear="1990";
-		person.new_group="Rob";
-		person.address2="City2 and street2";
-		person.phone2="688251";
-		fillPersonForm(person);
+		person.firstname = "Name";
+		person.lastname = "Surname";
+		person.address = "City and street";
+		// person.home = "569825";
+		person.home = phoneRand(); // random
+		// person.mobile = "29588991";
+		person.mobile = phoneRand();// random
+		// person.work = "744115";
+		person.work = phoneRand();// random
+		person.email = "test@mail.com";
+		person.email2 = "test2@mail.com";
+		// person.bday="28";
+		person.bday = bdayRand(); // random from list
+		// person.bmonth="April";
+		person.bmonth = bmonthRand(); // random from list
+		// person.byear = "1990";
+		person.byear = byearRand(); // random
+		// person.new_group = "Rob";
+		person.new_group = new_groupRand();// random from list
+		person.address2 = "City2 and street2";
+		// person.phone2 = "688251";
+		person.phone2 = phoneRand();// random fillPersonForm(person);
 		submitItemCreation();
 		returnToAddNewPage();
 	}
@@ -37,5 +44,4 @@ public class PersonCreationTests extends TestBase {
 		submitItemCreation();
 		returnToAddNewPage();
 	}
-
 }
--- src/com/example/tests/TestBase.java
+++ src/com/example/tests/TestBase.java
@@ -2,6 +2,7 @@ package com.example.tests;
 
 import static org.junit.Assert.fail;
 
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.Alert;
@@ -9,11 +10,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeTest;
 
 public class TestBase {
@@ -69,46 +69,105 @@ public class TestBase {
 		driver.findElement(By.linkText("group page")).click();
 	}
 
-	// block for add new tests	
+	// block for add new tests
 	protected void gotoAddNewPage() {
 		driver.findElement(By.linkText("add new")).click();
 	}
 
 	protected void fillPersonForm(PersonData person) {
 		driver.findElement(By.name("firstname")).clear();
-	    driver.findElement(By.name("firstname")).sendKeys(person.firstname);
-	    driver.findElement(By.name("lastname")).clear();
-	    driver.findElement(By.name("lastname")).sendKeys(person.lastname);
-	    driver.findElement(By.name("address")).clear();
-	    driver.findElement(By.name("address")).sendKeys(person.address);
-	    driver.findElement(By.name("home")).clear();
-	    driver.findElement(By.name("home")).sendKeys(person.home);
-	    driver.findElement(By.name("mobile")).clear();
-	    driver.findElement(By.name("mobile")).sendKeys(person.mobile);
-	    driver.findElement(By.name("work")).clear();
-	    driver.findElement(By.name("work")).sendKeys(person.work);
-	    driver.findElement(By.name("email")).clear();
-	    driver.findElement(By.name("email")).sendKeys(person.email);
-	    driver.findElement(By.name("email2")).clear();
-	    driver.findElement(By.name("email2")).sendKeys(person.email2);
-	    new Select(driver.findElement(By.name("bday"))).selectByVisibleText(person.bday);
-	    new Select(driver.findElement(By.name("bmonth"))).selectByVisibleText(person.bmonth);
-	    driver.findElement(By.name("byear")).clear();
-	    driver.findElement(By.name("byear")).sendKeys(person.byear);
-	    new Select(driver.findElement(By.name("new_group"))).selectByVisibleText(person.new_group);
-	    driver.findElement(By.name("address2")).clear();
-	    driver.findElement(By.name("address2")).sendKeys(person.address2);
-	    driver.findElement(By.name("phone2")).clear();
-	    driver.findElement(By.name("phone2")).sendKeys(person.phone2);
+		driver.findElement(By.name("firstname")).sendKeys(person.firstname);
+		driver.findElement(By.name("lastname")).clear();
+		driver.findElement(By.name("lastname")).sendKeys(person.lastname);
+		driver.findElement(By.name("address")).clear();
+		driver.findElement(By.name("address")).sendKeys(person.address);
+		driver.findElement(By.name("home")).clear();
+		driver.findElement(By.name("home")).sendKeys(person.home);
+		driver.findElement(By.name("mobile")).clear();
+		driver.findElement(By.name("mobile")).sendKeys(person.mobile);
+		driver.findElement(By.name("work")).clear();
+		driver.findElement(By.name("work")).sendKeys(person.work);
+		driver.findElement(By.name("email")).clear();
+		driver.findElement(By.name("email")).sendKeys(person.email);
+		driver.findElement(By.name("email2")).clear();
+		driver.findElement(By.name("email2")).sendKeys(person.email2);
+		new Select(driver.findElement(By.name("bday")))
+				.selectByVisibleText(person.bday);
+		new Select(driver.findElement(By.name("bmonth")))
+				.selectByVisibleText(person.bmonth);
+		driver.findElement(By.name("byear")).clear();
+		driver.findElement(By.name("byear")).sendKeys(person.byear);
+		new Select(driver.findElement(By.name("new_group")))
+				.selectByVisibleText(person.new_group);
+		driver.findElement(By.name("address2")).clear();
+		driver.findElement(By.name("address2")).sendKeys(person.address2);
+		driver.findElement(By.name("phone2")).clear();
+		driver.findElement(By.name("phone2")).sendKeys(person.phone2);
 	}
-	
+
 	protected void returnToAddNewPage() {
 		driver.findElement(By.linkText("add next")).click();
 	}
+
+	public String bdayRand() {
+		String bday = "-";
+
+		WebElement element = driver.findElement(By.name("bday"));
+		Select select = new Select(element);
+		Random rnd = new Random();
+		int index = rnd.nextInt(31);
+		bday = select.getOptions().get(index + 1).getText();
+
+		return bday;
+	}
+
+	public String bmonthRand() {
+		String bmonth = "-";
+
+		WebElement element = driver.findElement(By.name("bmonth"));
+		Select select = new Select(element);
+		Random rnd = new Random();
+		int index = rnd.nextInt(12);
+		
+		bmonth = select.getOptions().get(index + 1).getText();
+
+		return bmonth;
+	}
+
+	public String byearRand() {
+		String byear = "-";		
+
+		java.util.Calendar calendar = java.util.Calendar.getInstance(
+				java.util.TimeZone.getDefault(), java.util.Locale.getDefault());
+		calendar.setTime(new java.util.Date());
+		Random rnd = new Random();
+		int year = rnd.nextInt(calendar.get(java.util.Calendar.YEAR) - 1900);
+		year += 1900;
+		byear = Integer.toString(year);
+		return byear;
+	}
+
+	public String new_groupRand() {
+		String group = "[none]";
+		WebElement element = driver.findElement(By.name("new_group"));
+
+		Select select = new Select(element);
+		Random rnd = new Random();
+		int index = rnd.nextInt(select.getOptions().size()-1);
+		
+		group = select.getOptions().get(index + 1).getText();
+		
+		return group;
+	}
 	
-/*	protected void submitPersonCreation() {
-		driver.findElement(By.name("submit")).click();
-	}*/
+	public String phoneRand() {
+		String phone = "";
+		Random rnd = new Random();
+		int year = rnd.nextInt(9899999);
+		year += 100000;
+		phone = Integer.toString(year);
+		return phone;
+	}
 
 	private boolean isElementPresent(By by) {
 		try {

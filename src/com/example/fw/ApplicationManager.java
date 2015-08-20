395226ff05afa12ca324cddbbe67f92cd1bd0b1d
Maryia Radkevich <maskaradm@gmail.com>
2015-08-20 21:57:09 +0300

    Revert "Task 13"
    This reverts commit a4628ee83e20e5b576988a7e16bfd98b9d0ef125.

--- src/com/example/fw/ApplicationManager.java
+++ src/com/example/fw/ApplicationManager.java
@@ -1,12 +1,9 @@
 package com.example.fw;
 
-import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.ie.InternetExplorerDriver;
 
 public class ApplicationManager {
 
@@ -17,24 +14,12 @@ public class ApplicationManager {
 	private GroupHelper groupHelper;
 	private ContactHelper contactHelper;
 	private PhoneHelper phoneHelper;
-	private Properties properties;
 
-	public ApplicationManager(Properties properties) {
-		this.properties = properties;
-		String browser = properties.getProperty("browser");
-		if ("firefox".equals(browser)){
-			driver = new FirefoxDriver();
-		}else if ("ie".equals(browser)){
-			driver = new InternetExplorerDriver(); 
-		} else if ("chrome".equals(browser)){
-			driver=new ChromeDriver();
-		} else {
-			throw new Error("Unsupported browser: "+browser);
-		}
-		
-		baseUrl = properties.getProperty("baseUrl");
+	public ApplicationManager() {
+		driver = new FirefoxDriver();
+		baseUrl = "http://localhost/";
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-		driver.get(baseUrl);
+		driver.get(baseUrl + "/addressbookv4.1.4/");
 	}
 
 	public void stop() {
--- src/com/example/tests/TestBase.java
+++ src/com/example/tests/TestBase.java
@@ -1,20 +1,16 @@
 package com.example.tests;
 
-import static com.example.tests.ContactDataGenerator.generateRandomContacts;
-import static com.example.tests.GroupDataGenerator.generateRandomGroups;
-
-import java.io.File;
-import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Properties;
 
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
 
 import com.example.fw.ApplicationManager;
+import static com.example.tests.ContactDataGenerator.generateRandomContacts;
+import static com.example.tests.GroupDataGenerator.generateRandomGroups;
 
 public class TestBase {
 
@@ -22,10 +18,7 @@ public class TestBase {
 
 	@BeforeTest
 	public void setUp() throws Exception {
-		String configFile = System.getProperty("configFile", "application.properties");
-		Properties properties = new Properties();
-		properties.load(new FileReader(new File(configFile)));
-		app = new ApplicationManager(properties);
+		app = new ApplicationManager();
 	}
 
 	@AfterTest
@@ -38,27 +31,24 @@ public class TestBase {
 		return wrapGroupsForDataProvider(generateRandomGroups(5)).iterator();
 	}
 
-	public static List<Object[]> wrapGroupsForDataProvider(
-			List<GroupData> groups) {
+	public static List<Object[]> wrapGroupsForDataProvider(List<GroupData> groups) {
 		List<Object[]> list = new ArrayList<Object[]>();
 		for (GroupData group : groups) {
-			list.add(new Object[] { group });
-		}
+			list.add(new Object[]{group});
+		}	
 		return list;
 	}
 
 	@DataProvider
 	public Iterator<Object[]> randomValidContactGenerator() {
-		return wrapContactsForDataProvider(generateRandomContacts(5))
-				.iterator();
+		return wrapContactsForDataProvider(generateRandomContacts(5)).iterator();
 	}
 
-	protected List<Object[]> wrapContactsForDataProvider(
-			List<ContactData> contacts) {
+	protected List<Object[]> wrapContactsForDataProvider(List<ContactData> contacts) {
 		List<Object[]> list = new ArrayList<Object[]>();
 		for (ContactData contact : contacts) {
-			list.add(new Object[] { contact });
-		}
+			list.add(new Object[]{contact});
+		}	
 		return list;
 	}
 }
